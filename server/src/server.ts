0083b9dc31f92eaeb0b49caee714f5931dcc7504
Guilherme Oliveira Aguiar <aguiaroguilherme@gmail.com>
2023-01-13 14:40:08 -0400

    A

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 1 };
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.count();
+
+    return { count: pools };
   });
 
   await fastify.listen({ port: 3333 });
