0b4cec6e03d44225a303f03f5166ed5eb59cbfbd
imGuii <122046578+imgui2304@users.noreply.github.com>
2023-01-14 19:41:04 -0400

    Was changed the function of server side props for static props
    Challenge

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home(props: HomeProps) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const [poolCountResponse, guessCountResponse, userCountResponse] =
     await Promise.all([
       api.get("pools/count"),
