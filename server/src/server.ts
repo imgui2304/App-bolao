0c4eb74ba57ad10bf4c7ea909d8dd8cb080575eb
Guilherme Oliveira Aguiar <aguiaroguilherme@gmail.com>
2023-01-14 16:14:38 -0400

    Feat

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 import cors from "@fastify/cors";
+import ShortUniqueId from "short-unique-id";
+import { string } from "zod/lib";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -19,11 +22,35 @@ async function bootstrap() {
     return { count: pools };
   });
 
-  fastify.post("/pools", async (req, res) => {
-    const { title } = req.body;
-    return { title };
+  fastify.get("/users/count", async () => {
+    const pools = await prisma.user.count();
+    return { count: pools };
   });
 
+  fastify.get("/guesses/count", async () => {
+    const pools = await prisma.guess.count();
+    return { count: pools };
+  });
+
+  fastify.post("/pools", async (req, res) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(req.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code,
+      },
+    });
+
+    return res.status(201).send({ code });
+  });
   await fastify.listen({ port: 3333 });
 }
 
